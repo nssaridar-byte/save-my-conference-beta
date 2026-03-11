4c9f5e22095bdeaae208635f7716bf70c6e92481
jad kobeissi <jadkoneissi@gmail.com>
2026-03-11 15:33:58 +0200

    feat: implement signup backend

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma"
+import { isEmpty } from "../isEmpty"
+import { hash } from "bcrypt"
+import { sign } from "jsonwebtoken"
+
+export async function POST(req: Request) {
+    try {
+        const { email, password, name } = await req.json()
+
+        if (!email || !password || isEmpty([email, password])) {
+            return new Response("Please provide email and password", { status: 400 })
+        }
+
+        const userCheck = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if (userCheck) return new Response("Email taken", { status: 409 })
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                password: await hash(password, 10),
+                name
+            }
+        })
+
+        const token = await sign({ name, user }, process.env.JWT_SECRET as string)
+
+        return Response.json({ token })
+    } catch (error: any) {
+        return new Response(error, { status: 500 })
+    }
+}
